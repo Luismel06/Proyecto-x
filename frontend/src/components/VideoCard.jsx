f68afbb6c6a61ae76be972f446b5c5a72023edab
Luismel06 <luismel809524@gmail.com>
2025-12-10 00:13:55 -0400

    ---

--- frontend/src/components/VideoCard.jsx
+++ frontend/src/components/VideoCard.jsx
@@ -26,7 +26,7 @@ export default function VideoCard({ video, onOpen }) {
         </div>
         <div style={{ textAlign: "right" }}>
           <div className="price-main">${video.precio}</div>
-          <div className="price-sub">One Time Payment</div>
+          <div className="price-sub">One-time Payment</div>
         </div>
       </div>
     </button>
